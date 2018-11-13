4d04b20e651e5c24fdbba0bad1d5e9bd395cbb34
lyh <894806814@qq.com>
2018-11-13 11:05:50 +0800

    sj3

---
+++ demo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *
+ * @author: lyh
+ * @date: 2018/11/13
+ */
+require_once './vendor/autoload.php';
+use Abstarfish\Starfish\Tools;
+Tools::Hello();
---
+++ src/Abstarfish/Starfish/Tools.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * @author: lyh
+ * @date: 2018/11/13
+ */
+
+namespace Abstarfish\Starfish;
+
+
+class Tools
+{
+    public function __construct()
+    {
+    }
+
+    public static function Hello()
+    {
+        echo "hello,world";
+    }
+}
